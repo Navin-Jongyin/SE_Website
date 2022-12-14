a0165d0f41b6c70da60e21327822f3f4ec225e86
Navin Jongyin <63011210@kmitl.ac.th>
2022-12-15 00:55:06 +0700

    edit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+var accs = document.querySelectorAll(".accordion_item");	
+
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -24,3 +26,9 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+accs.forEach((accordion_item) => {
+    accordion_item.addEventListener("click", () => {
+        accordion_item.classList.toggle("active");
+    })
+})
